44f504fa620895e2326cb98795dc5cdd2f4afacf
Roman Tsukanov <roman@tsukanov.me>
2013-07-15 08:03:25 -0700

    Updated text on homepage.

--- core/views/index/index.php
+++ core/views/index/index.php
@@ -1,6 +1,6 @@
 <div class="alert">
     <button type="button" class="close" data-dismiss="alert">×</button>
-    <strong>Warning!</strong> This website is in development. Most features aren't working properly or not
-    implemented yet. Website may be very slow, because it's hosted on my local PC and connection isn't the best.
-    Sorry.
+    <strong>Warning! This website is in development. Most features aren't working properly or not
+    implemented yet.</strong>
+    <br />Website may be very slow. It is hosted on my Linux box at home and connection isn't the best.
 </div>
