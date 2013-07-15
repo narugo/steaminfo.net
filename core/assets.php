bfc8a52870f3e6cdd5a74d570d9d5cb6b1de6b80
Roman Tsukanov <roman@tsukanov.me>
2013-07-15 08:02:29 -0700

    Updated assets.

--- core/assets.php
+++ core/assets.php
@@ -9,7 +9,7 @@ define('PATH_TO_CSS', STATIC_DIR_URL . 'css/');
 define('JS_JQUERY', '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js');
 define('JS_JQUERY_UI', '//ajax.aspnetcdn.com/ajax/jquery.ui/1.10.2/jquery-ui.min.js');
 define('JS_D3', '//cdnjs.cloudflare.com/ajax/libs/d3/3.0.8/d3.min.js');
-define('JS_BOOTSTRAP', '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js');
+define('JS_BOOTSTRAP', '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js');
 define('JS_JQUERY_UI_AUTOCOMPLETE_HTML', PATH_TO_JS . 'jquery.ui.autocomplete.html.js');
 define('JS_TABLESORTER', PATH_TO_JS . 'jquery.tablesorter.min.js');
 
@@ -17,9 +17,9 @@ define('JS_TABLESORTER', PATH_TO_JS . 'jquery.tablesorter.min.js');
  * CSS
  */
 define('CSS_FONT_ROBOTO', '//fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700');
-define('CSS_FONT_AWESOME', '//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.min.css');
+define('CSS_FONT_AWESOME', '//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css');
 define('CSS_JQUERY_UI', '//ajax.aspnetcdn.com/ajax/jquery.ui/1.10.2/themes/cupertino/jquery-ui.css');
-define('CSS_BOOTSTRAP', '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css');
+define('CSS_BOOTSTRAP', '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css');
 define('CSS_MAIN', PATH_TO_CSS . 'screen.css');
 define('CSS_INDEX', PATH_TO_CSS . 'index.css');
 define('CSS_USERS', PATH_TO_CSS . 'users.css');
